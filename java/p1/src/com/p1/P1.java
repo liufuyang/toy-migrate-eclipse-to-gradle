af4811a90c3527a50bdc8acb76e05504c21cb9fb
Fuyang Liu <liufuyang@users.noreply.github.com>
2023-12-10 08:44:05 +0100

    init

---
+++ java/p1/src/com/p1/P1.java
@@ -0,0 +1,10 @@
+package com.p1;
+
+public class P1 {
+	
+	public P1() {}
+
+	public void sayHello() {
+		System.out.println("P1 says hellow");
+	}
+}
---
+++ java/p2/src/com/p2/P2.java
@@ -0,0 +1,13 @@
+package com.p2;
+
+import com.p1.P1;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		P1 p1 = new P1();
+		p1.sayHello();
+	}
+
+}
